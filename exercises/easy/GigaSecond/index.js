60efdf6f992a967182fcb5bd09f789e38160b581
Sandris Artemjevs <sandris.artemjevs@wandoofinance.com>
2019-07-14 09:08:58 +0300

    added more easy level exercises

---
+++ exercises/easy/GigaSecond/index.js
@@ -0,0 +1,10 @@
+/**
+ * Gigasecond
+ *
+ * Given a moment, determine the moment that would be after a gigasecond has passed.
+ * A gigasecond is 10^9 (1,000,000,000) seconds.
+ */
+
+function gigasecond(date) {}
+
+module.exports = gigasecond;
---
+++ exercises/easy/TwoFer/index.js
@@ -0,0 +1,17 @@
+/**
+ * Two-fer or 2-fer is short for two for one. One for you and one for me.
+ *
+ * Given a name, return a string with the message: One for X, one for me.
+ * Where X is the given name.
+ *
+ * However, if the name is missing, return the string: One for you, one for me.
+ *
+ * Examples:
+ * twoFer("Alice") === "One for Alice, one for me."
+ * twoFer("Bob") === "One for Bob, one for me."
+ * twoFer() === "One for you, one for me."
+ */
+
+function twoFer(name) {}
+
+module.exports = twoFer;
